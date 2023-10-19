d52c1cbc7978e39b8c96445bc1f656e548165170
saitadikonda99 <saiitadikonda7@gmail.com>
2023-10-19 20:32:22 +0530

    css updated

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useAuth } from '../Auth/AuthContext';
 import { useNavigate } from 'react-router-dom'; // Import useNavigate for redirection
+import './Login.css';
 
 function Login() {
   const { login, isAuthenticated } = useAuth();
@@ -57,19 +58,30 @@ function Login() {
   }
 
   return (
-    <div>
-      <h2>Login</h2>
+    <div className="login-form">
+    <div className="login-form-in"> 
       <form onSubmit={handleLogin}>
         <div>
-          <input type="text" name="user_name" placeholder="Username" onChange={handleChange} />
+          <input
+            type="text"
+            name="user_name"
+            placeholder='Username'
+            onChange={handleChange}
+          />
         </div>
         <div>
-          <input type="password" name="user_password" placeholder="Password" onChange={handleChange} />
+          <input
+            type="password"
+            name="user_password"
+            placeholder='Password'
+            onChange={handleChange}
+          />
         </div>
         <button type="submit">Login</button>
       </form>
-      {error && <p>{error}</p>}
     </div>
+  {error && <p>{error}</p>}
+</div>
   );
 }
 
--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import './Register.css';
 
 class RegistrationForm extends Component {
   constructor(props) {
@@ -38,34 +39,53 @@ class RegistrationForm extends Component {
 
   render() {
     return (
-      <div>
-        <h2>Register a New User</h2>
-        <form onSubmit={this.handleSubmit}>
-          <div>
-            <label>Username:</label>
-            <input type="text" name="user_name" value={this.state.user_name} onChange={this.handleInputChange} />
-          </div>
-
-          <div>
-            <label>Password:</label>
-            <input type="password" name="user_password" value={this.state.user_password} onChange={this.handleInputChange} />
-          </div>
-
-          <div>
-            <label>User Role:</label>
-            <select name="user_role" value={this.state.user_role} onChange={this.handleInputChange}>
-              <option value="admin">Admin</option>
-              <option value="clublead">Club Lead</option>
-              <option value="staff">Staff</option>
-              <option value="student">Student</option>
-            </select>
-          </div>
-
-          <button type="submit">Register</button>
-        </form>
+      <div className="registration-form">
+        <div className="registration-form-in">
+          <h2 className="form-title">Register a New User</h2>
+          <form className="user-form" onSubmit={this.handleSubmit}>
+            <div className="form-group">
+              <label className="form-label" for="user_name">Username:</label>
+              <input
+                type="text"
+                className="form-input"
+                name="user_name"
+                value={this.state.user_name}
+                onChange={this.handleInputChange}
+              />
+            </div>
+    
+            <div className="form-group">
+              <label className="form-label" for="user_password">Password:</label>
+              <input
+                type="password"
+                className="form-input"
+                name="user_password"
+                value={this.state.user_password}
+                onChange={this.handleInputChange}
+              />
+            </div>
+    
+            <div className="form-group">
+              <label className="form-label" for="user_role">User Role:</label>
+              <select
+                name="user_role"
+                className="form-select"
+                value={this.state.user_role}
+                onChange={this.handleInputChange}
+              >
+                <option value="admin">Admin</option>
+                <option value="clublead">Club Lead</option>
+                <option value="staff">Staff</option>
+                <option value="student">Student</option>
+              </select>
+            </div>
+    
+            <button type="submit" className="submit-button">Register</button>
+          </form>
+        </div>
       </div>
     );
   }
-}
+}  
 
 export default RegistrationForm;
